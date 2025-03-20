9f09ff58755ac640867313d20041383cfb17fcc2
Vinaykumar-Obillisetty <vinaykumar.vinay@accenture.com>
2025-03-20 13:12:32 +0530

    second commet

---
+++ HonsonDemo_03/src/test/java/Demo_03/ExtendReporter.java
@@ -0,0 +1,16 @@
+package Demo_03;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ExtendReporter{
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+
+}
